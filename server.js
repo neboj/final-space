d9e69c19211d593df202acc9458f081b66c7394b
boki <boki@bokis-MacBook-Pro.local>
2022-01-13 13:24:36 +0100

    Routes fix for prod.

--- server.js
+++ server.js
@@ -3,5 +3,9 @@ const serveStatic = require('serve-static');
 const path = require('path');
 app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port);
+
+app.route('/*').get(function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
